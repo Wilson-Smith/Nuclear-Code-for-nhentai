53adb1be00c972aa65086f828c81497dd5ba785e
Wilson Smith <donatosuozziwork@gmail.com>
2022-12-21 21:22:01 +0100

    Update Background.py

--- nhentai-nuclear/Background.py
+++ nhentai-nuclear/Background.py
@@ -2,24 +2,19 @@ def button():
     import random
     import requests
     import os
-    
-    def Generate_Code():
-        # Creation of the Code
+
+    while True:
+
+        # Creation of the code
         code = ''
         for i in range(6):
             code += str(random.randint(0, 9))
+        
         # Creation of the link
         url = 'https://nhentai.to/g/'+ code +'/'
-        return url
-    Generate_Code()
+        r = requests.get(url)
 
-    url=Generate_Code()
-
-    # Receive the status code
-    r = requests.get(url)
-
-    # Check if the status code is not 404
-    if r.status_code != 404:
-        os.system('start chrome --incognito '+'"'+url+'"')
-    else:
-        print("Restart")
+        # Launching Chrome if there is a real hentai
+        if r.status_code != 404:
+            os.system('start chrome --incognito '+'"'+ url +'"')
+            break
