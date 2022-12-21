44b98fe3292b4a845a47bf947fb2861d9699d494
Wilson Smith <donatosuozziwork@gmail.com>
2022-12-21 21:21:14 +0100

    This is the new version
    This is the new version and it was fixed the bug number #14 and #13.

--- nhentai-nuclear/Test_Background.py
+++ nhentai-nuclear/Test_Background.py
@@ -1,49 +1,20 @@
 def button():
-    # from selenium import webdriver
     import random
     import requests
-    # import webbrowser
     import os
-    
-    def Generate_Code():
-        # Creation of the Code
+
+    while True:
+
+        # Creation of the code
         code = ''
         for i in range(6):
             code += str(random.randint(0, 9))
+        
         # Creation of the link
         url = 'https://nhentai.to/g/'+ code +'/'
-        return url
-    Generate_Code()
+        r = requests.get(url)
 
-    url=Generate_Code()
-
-    # Receive the status code
-    r = requests.get(url)
-
-    # Check if the status code is not 404
-    if r.status_code != 404:
-        """TODO: Open link in incognito mode"""
-        # Opens Chrome in Incognito
-        """
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument("--incognito")
-        driver = webdriver.Chrome(chrome_options=chrome_options)
-        driver.get(url)
-        """
-        """
-        options = webdriver.ChromeOptions()
-        options.add_experimental_option("excludeSwitches", ["enable-logging"])
-        options.add_argument("--incognito")
-        driver = webdriver.Chrome(options=options)
-        driver.get(url)
-        driver.close()
-
-        while True:
-            pass
-        """
-        # Opens the link
-        # webbrowser.open_new_tab(url)
-
-        os.system('start chrome --incognito '+'"'+url+'"')
-    else:
-        print("Restart")
+        # Launching Chrome if there is a real hentai
+        if r.status_code != 404:
+            os.system('start chrome --incognito '+'"'+ url +'"')
+            break
